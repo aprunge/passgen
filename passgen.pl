04c9c8a0fbad92e8fed8a4342fe62bf2f8c47a19
aprung <93715513+aprunge@users.noreply.github.com>
2023-03-13 17:38:17 +0000

    Added special characters throughout password

--- passgen.pl
+++ passgen.pl
@@ -8,7 +8,9 @@ my $arg1 = int($ARGV[0]);
 
 if($arg1 == NULL) {
     die "Usage: perl passgen.pl <arg1>...\n";
-  }
+}
+
+my @special_chars = ('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', '\\', ';', ':', '\'', '\"', ',', '.', '/', '<', '>', '?');
 
 my $start_time = gettimeofday();
 
@@ -16,7 +18,15 @@ my $start_time = gettimeofday();
 my $word = Crypt::RandPasswd->word($arg1, $arg1);
 $word =~ s/(.)/int(rand(2)) ? uc($1) : lc($1)/eg;
 $word = ucfirst($word);
-print $word;
+
+my $word_with_special_chars = '';
+for (my $i = 0; $i < length($word); $i++) {
+    $word_with_special_chars .= substr($word, $i, 1);
+    if (int(rand(2))) {
+        $word_with_special_chars .= $special_chars[int(rand(@special_chars))];
+    }
+}
+print $word_with_special_chars;
 
 my $end_time = gettimeofday();
 my $elapsed_time = ($end_time - $start_time) * 1000;
