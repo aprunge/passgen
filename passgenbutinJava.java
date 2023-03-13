0ce7d3b8dae7289f3d4f1e9931ca2f5236c628a0
Dylan Flack <114887672+dylanflack@users.noreply.github.com>
2023-03-13 15:19:30 -0700

    w

---
+++ passgenbutinJava.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+
+public class passgenbutinJava {
+
+    public static void main(String[] args) {
+        
+        //array of characters for the program to chooese from
+        char[] chars = "dn".toCharArray();
+
+        //string builder to hold the password <-- what this guy said.
+        StringBuilder sb = new StringBuilder();
+
+        //randomize the characters
+        Random random = new Random();
+
+        //loops through the characters and to make a super secure password
+        for (int i = 0; i < 2; i++) {
+            char c = chars[random.nextInt(chars.length)];
+            sb.append(c);
+        }
+        //prints the super secure password
+        System.out.println(sb.toString());
+      }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* public class fuckYou {
+        public static void main(String[] args) {
+            System.out.println("dn");
+        }
+
+} */
